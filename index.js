525b05db4b9b49aad1411ff29289761cc70589a2
Andrei <rabota.laptev@mail.ru>
2018-11-15 13:07:44 +0700

    test post

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
+
+app.set('view engine','ejs');
+const data = 'Hallow, stupid World!!! ;-) !!!';
+const arr = ['Hello', 'World', 'Test'];
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('index');
 });
+app.use(bodyParser.urlencoded({extend: true}));
+
+
+app.get('/create', function (req, res) {
+    res.render('create',{data:data , arr:arr});
+
+});
+
+app.get('/create', function (req, res) {
+    res.render('create');
+});
+app.post('/create', function (req, res) {
+    console.log(req.body.text);
+});
+
+
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
